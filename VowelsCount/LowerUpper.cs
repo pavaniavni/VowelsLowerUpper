ec174b96da38dbbd712bdb61a002b6cdf9d87a6f
pavani <pavaniavni456@gmail.com>
2022-02-14 12:11:43 +0530

    added vowelsconsonantcountandlowertoupper

---
+++ VowelsCount/LowerUpper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VowelsCount
+{
+    internal class LowerUpper
+    {
+       
+        public static void Main()
+        {
+            string st;
+            char[] a;
+            int len, i;
+            len = 0;
+            char ch;
+            Console.WriteLine("Replace LowerCase Characters by UpperCase and UpperCase Characters by LowerCase  :");
+            Console.WriteLine(" ");
+            Console.Write("Enter the string : ");
+            st = Console.ReadLine();
+            len = st.Length;
+            a = st.ToCharArray(0, len); 
+
+            Console.Write("\nAfter conversion, the string is : ");
+            for (i = 0; i < len; i++)
+            {
+                ch = a[i];
+                if (Char.IsLower(ch))
+                {
+                    Console.Write(Char.ToUpper(ch));
+                }
+                else
+                {
+                    Console.Write(Char.ToLower(ch));
+                }
+            }
+            Console.WriteLine(" ");
+        }
+    }
+}
+
---
+++ VowelsCount/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+public class VowelsCo
+{
+    public static void Main1()
+    {
+        string s;
+        Console.WriteLine("Enter the string : ");
+        Console.WriteLine("..................");
+        s = Console.ReadLine();
+        int len = s.Length;
+        int vow = 0, con = 0;
+        for (int i = 0; i < len; i++)
+        {
+
+            if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U')
+            {
+                vow++;
+            }
+            else
+            {
+                con++;
+            }
+        }
+        Console.WriteLine("The Total number of vowel in the string is : {0}", vow);
+        Console.WriteLine("The Total number of consonant in the string is : {0}\n\n", con);
+    }
+}
